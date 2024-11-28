7b5805bda2163e7885dbf6d10e68fdd32014c87d
Yawen Li <lhf552004@gmail.com>
2024-11-28 13:04:19 -0330

    change ecr to data

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,10 +1,10 @@
 # Create ECR Repository
-resource "aws_ecr_repository" "this" {
+data "aws_ecr_repository" "this" {
   name = var.name
 }
 
 resource "aws_ecr_repository_policy" "lambda_access_policy" {
-  repository = aws_ecr_repository.this.name
+  repository = data.aws_ecr_repository.this.name
   policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,8 +1,8 @@
 output "repository_url" {
-  value = aws_ecr_repository.this.repository_url
+  value = data.aws_ecr_repository.this.repository_url
 }
 
 output "repository_name" {
   description = "The name of the ECR repository"
-  value       = aws_ecr_repository.this.name
+  value       = data.aws_ecr_repository.this.name
 }
